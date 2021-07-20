f90c443aa35dadd2bd2c51acc964f9b3e1908bdb
Samuel Kurian <samuelrkurian@gmail.com>
2021-07-20 19:46:45 +1200

    Specified date format

--- main.py
+++ main.py
@@ -15,68 +15,13 @@ from filter import Filter
 D0 = datetime(2000, 1, 1)
 
 
-class TaskGenerator():
-    def __init__(self):
-
-        d_today = datetime.now()        # today's date
-
-        self.delta = getDateDelta(d_today)
-        print("Now: ", self.delta)
-
-        self.addition()
-
-    def addition(self):
-        self.task_list = []
-
-        n = 1
-        task = input(f'Task {n} (blank to cancel): ')
-
-        while task != '':
-            task_info = {
-                'id': n,
-                'details': task,
-                'due': str(input('Due: ')),
-                'hours_needed': int(input('Hours Needed: ')),
-                'gradient': input('Gradient (+/-/0): ')
-            }
-            self.task_list.append(task_info)
-            n = n+1
-            print(task_info)
-
-            task = input(f'Task {n} (blank to cancel): ')
-
-        self.save()
-        self.work_to_do()
-
-    def save(self):
-        if self.task_list != []:
-            # with open('data.json', 'r') as json_file:
-            #     data = json.load(json_file)
-            with open('data.json', 'w') as outfile:
-                json.dump(self.task_list, outfile)
-
-    def work_to_do(self):
-        self.dates = {}
-
-        with open('data.json') as json_file:
-            data = json.load(json_file)
-
-        for n in data:
-            due_date = getDateDelta(n['due'])
-            # if (due_date - datetime.now()).days > 0 and (due_date - datetime.now()).days <= n['hours_needed']:
-            #     work_start_delta = (due_date - D0).days - n['hours_needed']
-            print(due_date)
-
-            # TaskModel(due=12, work=10, week_day_work=6)
-
-
 def dummy_start():
     n = new_id()
     task_cumulation = {}
     name = input(f'Task No. {n} Name (blank to cancel): ')
 
     while name != "":
-        d = getDateDelta(input('Due date: '))
+        d = getDateDelta(input('Due date (yyyy-mm-dd):'))
         w = int(input('Hrs of work: '))
         g = input('Gradient (+/-/0): ')
 
