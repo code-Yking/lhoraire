7894e6de2ca05d4392e369cf2950a7068d41f55c
Samuel Kurian <samuelrkurian@gmail.com>
2021-08-03 19:41:53 +1200

    Cleaned the code

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ import json
 from json.decoder import JSONDecodeError
 import math
 
-# from sympy.solvers import solve
-# from sympy import Symbol, N, integrate, expand
-
-
 from model import TaskModel
-# from reposition import Reposition
 from helpers import getDateDelta
 from filter import Filter
 
@@ -21,7 +16,7 @@ def dummy_start():
     name = input(f'Task No. {n} Name (blank to cancel): ')
 
     while name != "":
-        d = getDateDelta(input('Due date (yyyy-mm-dd):'))
+        d = getDateDelta(input('Due date (yyyy-mm-dd): '))
         w = int(input('Hrs of work: '))
         g = input('Gradient (+/-/0): ')
 
@@ -33,21 +28,19 @@ def dummy_start():
         n = n+1
         name = input(f'Task No. {n} Name (blank to cancel): ')
 
-    # a = Reposition(task_cumulation, 6, 10)
-    a = Filter(task_cumulation)
+    Filter(task_cumulation)
 
 
 def new_id():
+    # produces new id after reading the used ids from file
     with open('tasks.json') as tasks_json:
         try:
             tasks = json.load(tasks_json)
             nos = sorted([int(id.strip('t'))
                           for id in tasks.keys()], reverse=True)
             if float(nos[0]).is_integer():
-                # print(int(nos[0]) + 1, nos)
                 return int(nos[0]) + 1
             else:
-                # print(math.ceil(float(nos[0])))
                 return math.ceil(float(nos[0]))
         except JSONDecodeError:
             # print(1)
@@ -55,10 +48,4 @@ def new_id():
 
 
 if __name__ == "__main__":
-    # d = int(input('Days to due: '))
-    # w = int(input('Hrs of work: '))
-    # task = TaskModel(due=d, work=w, week_day_work=6, days=0, gradient='0')
-    # print(task.generate_list())
     dummy_start()
-    # new_id()
-    # TaskGenerator()
